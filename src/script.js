0f9baac6a99da2bddac41da952acd1d12a5853f8
dalailahner <59581086+dalailahner@users.noreply.github.com>
2025-01-30 17:03:08 +0100

    setup basic elements

--- src/script.js
+++ src/script.js
@@ -1,1 +1,21 @@
+console.clear();
+
 console.log("Hello!");
+
+// advanced tab
+document.querySelector(".advancedBtn").addEventListener("click", (ev) => {
+  document.querySelector(".advancedCont").classList.toggle("open");
+});
+
+// form submit
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  const formData = new FormData(form);
+  let output = "";
+  for (const entry of formData) {
+    output = `${output}${entry[0]} = ${entry[1]}\r`;
+  }
+  console.log(output);
+});
